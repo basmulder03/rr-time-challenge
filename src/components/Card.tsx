03f95737f730b7cdc5754470f0886f3d9856eeca
Bas Mulder <bas.d.mulder@gmail.com>
2023-10-05 21:11:12 +0200

    update

---
+++ src/components/Card.tsx
@@ -0,0 +1,11 @@
+const Card = () => {
+    return (
+        <div>
+            <div className="h-36 w-28 m-4 bg-slate-200 rounded-lg text-center" >
+                test
+            </div>
+        </div>
+    )
+}
+
+export default Card;
